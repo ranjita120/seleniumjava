fcd5584a7f46c7cfee7255014136cbfe5f5c6ad7
ranjitha <ish.ranjitha26@gmail.com>
2021-05-17 01:49:24 +0530

    added log statement to calender

--- src/main/java/com/udemy/Calender.java
+++ src/main/java/com/udemy/Calender.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 
 public class Calender {
     public static void main(String[] args) {
+        System.out.println("args = " + java.util.Arrays.deepToString(args));
         Calendar c1=Calendar.getInstance();
         SimpleDateFormat s1=new SimpleDateFormat("M/dd/yyyy");
         System.out.println(s1.format(c1.getTime()));
